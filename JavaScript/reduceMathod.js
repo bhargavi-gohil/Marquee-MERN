c212ee8f7c298b144c4a602d1c9fc6541a3b4cbf
bhargavi-gohil <bhargavigohil098@gmail.com>
2026-07-08 13:40:52 +0530

    Reduce method use for finding totalMarks in array..

--- JavaScript/reduceMathod.js
+++ JavaScript/reduceMathod.js
@@ -1,6 +1,16 @@
 // WAP to get the total marks using reduce() method
 // and then get the percentage..
 
-const marks = [96,78,90,78,59
-    
-]
+
+const marks = [96,88,90,98,100]
+
+const total = marks.reduce((accumulator, currentValue, index) => {
+    return accumulator + currentValue;
+}, 0);
+
+const len = (marks.length)*100;
+const percentage = marks.reduce((accumulator, currentValue, index) => {
+    return (total/len)*100;
+}, 0);
+
+console.log(` total Marks ${total} and percentage ${percentage}%`);
