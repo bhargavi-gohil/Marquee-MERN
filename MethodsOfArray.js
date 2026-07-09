f784966cb1c2b3e860ea7b7869b870cd658b7dcc
bhargavi-gohil <bhargavigohil098@gmail.com>
2026-07-09 13:20:30 +0530

    methods of array

---
+++ MethodsOfArray.js
@@ -0,0 +1,14 @@
+// values():
+
+let num = [1,2,3,4,5]
+// console.log(num.values())
+
+// keys():
+
+console.log(num.keys())
+
+// splice():
+
+let arr = [1,2,3,4,5]
+arr.splice(2,1,"Hello")
+console.log(arr)
